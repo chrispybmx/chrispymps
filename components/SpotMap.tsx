b27cc14ad2fbcaf6e321b663b090b426d4c16b99
chrispybmx <christian.ceresato@gmail.com>
2026-04-27 14:22:50 +0200

    ui: switch map tiles to CartoDB Dark Matter

--- components/SpotMap.tsx
+++ components/SpotMap.tsx
@@ -174,8 +174,9 @@ export default function SpotMap({
         zoomControl: false,
       });
 
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '© <a href="https://openstreetmap.org/copyright">OpenStreetMap</a>',
+      L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+        attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        subdomains: 'abcd',
         maxZoom: 19, className: 'osm-tiles',
       }).addTo(map);
 
